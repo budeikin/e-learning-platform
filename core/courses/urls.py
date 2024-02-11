83af17e30cb12f33731bfde3fe5df59c47c2c5f3
budeikin <budeikin52@gmail.com>
2024-02-11 11:07:52 +0330

    adding content list for modules

--- core/courses/urls.py
+++ core/courses/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import ManageCourseListView, CourseCreateView, CourseUpdateView, CourseDeleteView, CourseModuleUpdateView, \
-    ContentCreateUpdateView, ContentDeleteView
+    ContentCreateUpdateView, ContentDeleteView, ModuleContentListView
 
 urlpatterns = [
     path('mine/', ManageCourseListView.as_view(), name='manage_course_list'),
@@ -13,4 +13,6 @@ urlpatterns = [
     path('module/<int:module_id>/content/<model_name>/<id>/', ContentCreateUpdateView.as_view(),
          name='module_content_update'),
     path('content/<int:id>/delete/', ContentDeleteView.as_view(), name='module_content_delete'),
+    path('module/<int:module_id>/', ModuleContentListView.as_view(),
+         name='module_content_list'),
 ]
