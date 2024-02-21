12f296d6d35c347c7c7a77e75e920db86d39ec12
budeikin <budeikin52@gmail.com>
2024-02-21 10:33:01 +0330

    adding CourseEnroll using APIView

--- core/courses/api/urls.py
+++ core/courses/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import SubjectListApiView, SubjectDetailApiView, CourseListView
+from .views import SubjectListApiView, SubjectDetailApiView, CourseListView, CourseEnrollView
 
 app_name = 'courses'
 
 urlpatterns = [
     path('subjects/', SubjectListApiView.as_view(), name='subject_list'),
     path('courses/', CourseListView.as_view(), name='course_list'),
-    path('subjects/<int:pk>/', SubjectDetailApiView.as_view(), name='subject_detail')
+    path('subjects/<int:pk>/', SubjectDetailApiView.as_view(), name='subject_detail'),
+    path('courses/<pk>/enroll/', CourseEnrollView.as_view(), name='course_enroll')
 ]
--- core/courses/api/views.py
+++ core/courses/api/views.py
@@ -1,6 +1,11 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .serializers import SubjectSerializer, CourseSerializer
 from courses.models import Subject, Course
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class SubjectListApiView(ListAPIView):
@@ -16,3 +21,13 @@ class SubjectDetailApiView(RetrieveAPIView):
 class CourseListView(ListAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+
+class CourseEnrollView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [BasicAuthentication]
+
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
