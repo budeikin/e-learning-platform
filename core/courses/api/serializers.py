78e3f4eec2635d8c0a584875ff06371001954f5e
budeikin <budeikin52@gmail.com>
2024-02-24 11:05:57 +0330

    adding permissions and new serilizers for courses

--- core/courses/api/serializers.py
+++ core/courses/api/serializers.py
@@ -1,5 +1,5 @@
-from rest_framework.serializers import ModelSerializer
-from courses.models import Subject, Course, Module
+from rest_framework.serializers import ModelSerializer, RelatedField
+from courses.models import Subject, Course, Module, Content
 
 
 class SubjectSerializer(ModelSerializer):
@@ -20,3 +20,34 @@ class CourseSerializer(ModelSerializer):
     class Meta:
         model = Course
         fields = ['id', 'subject', 'title', 'slug', 'overview', 'created', 'owner', 'modules']
+
+
+class ItemRelatedField(RelatedField):
+    def to_representation(self, value):
+        return value.render()
+
+
+class ContentSerializer(ModelSerializer):
+    content = ItemRelatedField(read_only=True)
+
+    class Meta:
+        model = Content
+        fields = ['order', 'content']
+
+
+class ModuleWithContentSerializer(ModelSerializer):
+    contents = ContentSerializer(many=True)
+
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description', 'contents']
+
+
+class CourseWithContentSerializer(ModelSerializer):
+    modules = ModuleWithContentSerializer(many=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug',
+                  'overview', 'created', 'owner',
+                  'modules']
